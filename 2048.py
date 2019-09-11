e7b8b2e383760d7ea84da37dea2565eeeeff6b1f
William Duncan <william.philip.duncan@gmail.com>
2019-09-11 12:33:46 -0700

    Initial commit.

---
+++ 2048.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Open browser to the game page.
+browser = webdriver.Firefox()
+browser.get('https://gabrielecirulli.github.io/2048/')
+
+htmlElem = browser.find_element_by_tag_name('html')
+
+# Introduce a counter for flavor.
+counter = 0
+
+# Trigger to continue or end play in while-loop later.
+def CheckIfOver():
+
+	try:
+		return browser.find_element_by_css_selector('.game-over')
+
+	except:
+		return None
+
+overElem = CheckIfOver()
+
+# while-loop to play through the game.
+while overElem is None:
+
+	# Move in a clockwise direction, forever.
+	htmlElem.send_keys(Keys.UP)
+	htmlElem.send_keys(Keys.RIGHT)
+	htmlElem.send_keys(Keys.DOWN)
+	htmlElem.send_keys(Keys.LEFT)
+
+	counter += 1
+
+	# Decide whether to continue or end.
+	overElem = CheckIfOver()
+
+print("Congratulations! Victory in only %s turns." % str(counter))
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # 2048-Game
- Play the 2048 game, and slide tiles together to reach the high score. This project automates that first part, by sliding in a scripted pattern. Designed for use with Al Sweigart's Automate the Boring Stuff with Python: Chapter 11.
+
+Play the 2048 game, and slide tiles together to reach the high score.
+
+This project automates that first part, by sliding in a scripted pattern.
+
+Designed for use with Al Sweigart's Automate the Boring Stuff with Python: Chapter 11.
